3afc92837ee97b4ce4f234aecf6b0a3995e02574
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2021-11-16 13:37:17 +0100

    Create Shape.cs

---
+++ Shape.cs
@@ -0,0 +1,10 @@
+namespace SharpEngine
+{
+    public class Shape
+    {
+        protected Shape(Vertex[] createRectangle, Material material)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
